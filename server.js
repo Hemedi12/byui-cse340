4d192a7aaf1f5a1298d1d5b61ec8910ee3929fd8
ITPro-Hemedi <hemedicharles@gmail.com>
2025-07-19 08:59:36 +0200

    updated file

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/") 
 
 /**********************************************
  *View Engine and Templates
@@ -25,7 +26,6 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 
-
 //index route
 // app.get("/", function(req, res){
 //   res.render("index", {title: "Home"})
@@ -33,6 +33,27 @@ app.use(static)
 app.get("/", baseController.buildHome)
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -46,3 +67,4 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+utilities.handleErrors(baseController.buildHome)
